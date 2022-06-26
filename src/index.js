9b7a752d7355988bf5088b474b3bb46c142e33f6
Artur Olenberg <olenberg.ai@gmail.com>
2022-06-26 17:46:30 +0600

    Create common logic

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+const startGame = (gameDescription, gameData) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameDescription);
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, rightAnswer] = gameData();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer.toLowerCase() === rightAnswer.toLowerCase()) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startGame;
