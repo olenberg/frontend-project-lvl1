84c2925a4c4b97402ce5e4883a39416e9110a01f
Artur Olenberg <olenberg.ai@gmail.com>
2022-06-26 17:42:06 +0600

    Create random generator

---
+++ src/utilities.js
@@ -0,0 +1,7 @@
+const randomGenerator = (min, max) => {
+  const minNumber = Math.ceil(min);
+  const maxNumber = Math.floor(max);
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+};
+
+export default randomGenerator;
