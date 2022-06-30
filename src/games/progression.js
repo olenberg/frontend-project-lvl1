a6166ddb482dffd0de32eae66a4febd7449de1d3
Artur Olenberg <olenberg.ai@gmail.com>
2022-06-30 21:04:36 +0600

    Refactor progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,21 +2,19 @@ import randomGenerator from '../utilities.js';
 
 export const gameDescription = 'What number is missing in the progression?';
 
-const getProgression = () => {
+const getProgression = (length, start, step) => {
   const progression = [];
-  const progressionLength = randomGenerator(5, 10);
-  const progressionStep = randomGenerator(1, 5);
-  let progressionElement = 0;
-
-  for (let i = 0; i < progressionLength; i += 1) {
-    progressionElement += progressionStep;
-    progression.push(progressionElement);
+  for (let i = 0; i <= length; i += 1) {
+    progression.push(start + (step * i));
   }
   return progression;
 };
 
 export const gameData = () => {
-  const progression = getProgression();
+  const progressionLength = randomGenerator(5, 10);
+  const progressionStart = randomGenerator(1, 10);
+  const progressionStep = randomGenerator(1, 5);
+  const progression = getProgression(progressionLength, progressionStart, progressionStep);
   const hiddenElement = randomGenerator(1, progression.length - 2);
   const correctAnswer = String(progression[hiddenElement]);
   progression[hiddenElement] = '..';
