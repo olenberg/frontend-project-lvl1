dcfbca22cce580676152d743a1e69295bb43c3ec
Artur Olenberg <olenberg.ai@gmail.com>
2022-06-30 21:04:59 +0600

    Refactor gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,12 +2,16 @@ import randomGenerator from '../utilities.js';
 
 export const gameDescription = 'Find the greatest common divisor of given numbers.';
 
-const getGCD = (firstNumber, secondNumber) => {
-  if (secondNumber) {
-    return getGCD(secondNumber, firstNumber % secondNumber);
+/*
+const getGCD = (first, second) => {
+  if (second) {
+    return getGCD(second, first % second);
   }
-  return firstNumber;
+  return first;
 };
+*/
+
+const getGCD = (first, second) => (second ? getGCD(second, first % second) : first);
 
 export const gameData = () => {
   const firstNumber = randomGenerator(1, 50);
